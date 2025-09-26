6ef48e1ebc4bef3b7cff41b6bee68d6675f65a6a
Chadi-Mangle <chadimangled@gmail.com>
2025-09-26 12:42:34 +0200

    add: create file utils

---
+++ file.go
@@ -0,0 +1,15 @@
+package main
+
+import "os"
+
+func saveBytesToFile(data []byte, filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	file.Write(data)
+
+	return nil
+}
